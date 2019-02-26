7cfc78db7bbfc49ea1c57f0b420ef2460adf4170
Solis <imsolis@hotmail.com>
2019-02-26 14:39:13 -0600

    Creación de una pantalla de loging, una BD con la tabla usuarios y un pantalla para dar de alta a estos.

---
+++ app/src/main/java/mx/uv/herbolario/DBHelper.java
@@ -0,0 +1,23 @@
+package mx.uv.herbolario;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+       public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
+           super(context, name, factory, version);
+       }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+            db.execSQL("create table usuarios(codigo integer primary key autoincrement,nombre text, usuario tex,contrasena text, tipo text)");
+            db.execSQL("insert into usuarios values(01,'admin','admin','admin')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("create table usuarios(codigo integer primary key autoincrement,nombre text, usuario tex,contrasena text, tipo text)");
+            db.execSQL("insert into usuarios values(01,'admin','admin','admin')");
+    }
+
+}
