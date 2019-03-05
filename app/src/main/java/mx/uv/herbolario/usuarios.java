eecfa082fcc5bae616704a97fb1409da889757cd
Solis <imsolis@hotmail.com>
2019-03-05 11:42:19 -0600

    Agregué menú al presionar elemento de la lista.

--- app/src/main/java/mx/uv/herbolario/usuarios.java
+++ app/src/main/java/mx/uv/herbolario/usuarios.java
@@ -4,9 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -20,6 +25,8 @@ public class usuarios extends AppCompatActivity {
     ListView listaUsuarios;
     ArrayAdapter<String> adapter;
     private UsuariosOperaciones usuariosdb;
+    private int seleccionado = -1;
+    private Object action;
 
     @SuppressLint("ResourceType")
     @Override
@@ -28,15 +35,54 @@ public class usuarios extends AppCompatActivity {
         setContentView(R.layout.activity_usuarios);
         usuariosdb = new UsuariosOperaciones(this);
         usuariosdb.open();
-        listaUsuarios =this.findViewById(R.id.listUsuarios);
+        listaUsuarios = this.findViewById(R.id.listUsuarios);
         LlenarListaUsuarios();
+        borrar();
     }
 
-    public void LlenarListaUsuarios(){
+    public void LlenarListaUsuarios() {
         List usuarios = usuariosdb.getAllUsuarios();
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, usuarios);
         listaUsuarios.setAdapter(adapter);
     }
+
+    public void borrar() {
+        listaUsuarios.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        listaUsuarios.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                seleccionado = position;
+                action = usuarios.this.startActionMode(amc);
+                view.setSelected(true);
+                return true;
+            }
+        });
+
+    }
+
+    private ActionMode.Callback amc = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            getMenuInflater().inflate(R.menu.opciones, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            return false;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+
+        }
+    };
 
     public void agregar(View view){
         Intent agregar= new Intent(this,AgregarUsuario.class);
